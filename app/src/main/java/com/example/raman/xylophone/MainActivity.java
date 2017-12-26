d700a5958fe694c3b3b77337787762cdd1cdbe3c
raman <ramanlfc@gmail.com>
2017-12-26 21:35:43 +0530

    wired up all ui elements and setup same click handler for all buttons

--- app/src/main/java/com/example/raman/xylophone/MainActivity.java
+++ app/src/main/java/com/example/raman/xylophone/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.raman.xylophone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button note1Button;
+    private Button note2Button;
+    private Button note3Button;
+    private Button note4Button;
+    private Button note5Button;
+    private Button note6Button;
+    private Button note7Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wireUpUIElements();
+    }
+
+    private void wireUpUIElements() {
+        note1Button = findViewById(R.id.note1Button);
+        note2Button = findViewById(R.id.note2Button);
+        note3Button = findViewById(R.id.note3Button);
+        note4Button = findViewById(R.id.note4Button);
+        note5Button = findViewById(R.id.note5Button);
+        note6Button = findViewById(R.id.note6Button);
+        note7Button = findViewById(R.id.note7Button);
+    }
+
+    public void playNote(View view) {
     }
 }
